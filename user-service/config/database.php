777ed58dfd153430870d7790b1c333d095aebc89
Edward Pasachoa <eypasachoa@uniboyac.edu.co>
2025-11-28 20:08:30 -0500

    creacion del database

---
+++ user-service/config/database.php
@@ -0,0 +1,17 @@
+<?php
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'vuelos_app',
+    'username' => 'root',  # XAMPP usa root sin contraseña por defecto
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
